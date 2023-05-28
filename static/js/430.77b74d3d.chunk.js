b92f59356c33da20d99c09c55ac39be4d1aed96b
Max3281 <113448092+Max3281@users.noreply.github.com>
2023-05-28 17:16:08 +0000

    Deploying to gh-pages from @ Max3281/goit-react-hw-08-phonebook@395ecfbf2508e95b36a8d7aa453cbef7d80821a0 🚀

---
+++ static/js/430.77b74d3d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[430],{2430:function(e,t,n){n.r(t);var o=n(184),r=n(1087).NavLink;t.default=function(){return(0,o.jsxs)("section",{children:[(0,o.jsx)("p",{children:"Unfortunately there is no such page. Please proceed to the Home page if you want to continue"}),(0,o.jsx)(r,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=430.77b74d3d.chunk.js.map
